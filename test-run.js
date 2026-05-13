d69ed8b8b179150d968fb94dcfdc9d05c7700792
notBlubbll <149101422+notBlubbll@users.noreply.github.com>
2026-05-14 00:01:37 +0200

    Delete test-run.js

--- test-run.js
+++
@@ -1,4 +0,0 @@
-process.on("uncaughtException", e => { process.stderr.write(`\n[test] uncaught: ${e.stack || e.message}\n`); process.exit(1); });
-process.on("unhandledRejection", e => { process.stderr.write(`\n[test] unhandled: ${e?.stack || e?.message}\n`); });
-process.on("exit", code => { process.stderr.write(`\n[test] exit code=${code}\n`); });
-import("./src/server.js").then(() => process.stderr.write("[test] import done\n")).catch(e => process.stderr.write(`[test] import fail: ${e.stack || e.message}\n`));
